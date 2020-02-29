083c30f349b4a5bd6666a82ad0bf72fb2bd51345
Sheerin Naseem <sheerinaseem@Sheerins-MacBook-Pro.local>
2020-02-29 15:11:00 +0530

    first commit

---
+++ Block_Breaker/Assets/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //config parameteres
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchonMouseClick();
+        }
+
+    }
+
+    private void LaunchonMouseClick()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ Block_Breaker/Assets/Script/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+
+	public void LoadMyScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+
+    public void LoadScenebyName(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
